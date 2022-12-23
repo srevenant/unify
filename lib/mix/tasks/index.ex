96bc2352bf0b4766059ee3efeddd2e76edc052cf
Brandon Gillespie <bjg@solv.com>
2022-12-23 08:21:15 -0700

    .

--- lib/mix/tasks/index.ex
+++ lib/mix/tasks/index.ex
@@ -6,45 +6,30 @@ defmodule Mix.Tasks.Rivet do
   @moduledoc """
   Prints Rivet tasks and their information.
 
-      $ mix rivet
+      $ mix rivet help|{cmd}
 
   """
 
   @impl true
-  def run(_) do
+  def run(args) do
+    case args do
+      [] -> list_commands()
+      ["help"] -> list_commands()
+      [cmd | args] ->
+        try do
+          Module.safe_concat(__MODULE__, Macro.camelize(cmd)).run(args)
+        rescue _ in ArgumentError ->
+          list_commands()
+          IO.puts(:stderr, "\nRivet command not found: `#{cmd}`\n")
+        end
+    end
+  end
+
+  defp list_commands() do
     Application.ensure_all_started(:rivet)
     Mix.shell().info("Rivet v#{Application.spec(:rivet, :vsn)}")
     Mix.shell().info("A toolkit for managing models in Elixir, working with Ecto.")
     Mix.shell().info("\nAvailable tasks:\n")
     Mix.Tasks.Help.run(["--search", "rivet."])
   end
-
-  # @impl true
-  # def run([cmd | args]) do
-  #   Module.safe_concat(__MODULE__, Macro.camelize(cmd)).run(args)
-  # rescue
-  #   _ in ArgumentError ->
-  #     list_commands()
-  #     IO.puts(:stderr, "\nRivet command not found: `#{cmd}`\n")
-  # end
-
-  # def run([]), do: list_commands()
-  #
-  # defp list_commands() do
-  #   cmd = Rivet.Mix.Common.task_cmd(__MODULE__)
-  #   IO.puts(:stderr, "\nSyntax:\n")
-  #
-  #   IO.inspect(:application.get_key(:rivet, :modules))
-  #   with {:ok, mods} <- :application.get_key(:rivet, :modules) do
-  #     Enum.each(mods, fn mod ->
-  #       case "#{mod}" do
-  #         "Elixir.Mix.Tasks.Rivet." <> sub ->
-  #           IO.puts(:stderr, "  mix #{cmd} #{Macro.underscore(sub)} #{mod.summary()}")
-  #
-  #         _ ->
-  #           :ok
-  #       end
-  #     end)
-  #   end
-  # end
 end
