4c3276d3c090895c0843bcf8fb25ecae9da4e62a
Brandon Gillespie <bjg@solv.com>
2023-04-17 11:28:26 -0600

    deps

--- mix.exs
+++ mix.exs
@@ -63,8 +63,7 @@ defmodule Rivet.MixProject do
       {:excoveralls, "~> 0.14", only: :test},
       {:mix_test_watch, "~> 0.8", only: [:test, :dev], runtime: false},
       {:postgrex, "~> 0.13", only: [:test]},
-      {:rivet_utils, "~> 1.0.3",
-       git: "https://github.com/srevenant/rivet-utils", branch: "master"},
+      {:rivet_utils, "~> 1.0.4"},
       {:transmogrify, "~> 1.1"},
       {:typed_ecto_schema, "~> 0.3.0 or ~> 0.4.1"},
       {:yaml_elixir, "~> 2.8.0"}
