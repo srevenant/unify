c4674b014107753ec214e5a0ca3b2ce828b3dc6a
Brandon Gillespie <bjg@solv.com>
2023-04-24 10:41:09 -0600

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rivet.MixProject do
   def project do
     [
       app: :rivet,
-      version: "1.0.3",
+      version: "1.0.4",
       elixir: "~> 1.13",
       description: "Elixir data model framework library",
       source_url: "https://github.com/srevenant/rivet",
