a7c04193da2446659c1a581911487f05bf80a9dd
Lyle Mantooth <lyle@menteeth.us>
2023-09-13 14:57:50 -0400

    Deps update (#11)
    * Update dependencies.
    
    * A wild typo appeared!
    
    * v1.15 updates
    
    ---------
    
    Co-authored-by: Brandon Gillespie <bjg@solv.com>

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Rivet.MixProject do
   def project do
     [
       app: :rivet,
-      version: "2.0.2",
-      elixir: "~> 1.13",
+      version: "2.1.0",
+      elixir: "~> 1.14",
       description: "Elixir data model framework library",
       source_url: "https://github.com/srevenant/rivet",
       docs: [main: "Rivet"],
@@ -52,14 +52,14 @@ defmodule Rivet.MixProject do
       {:ecto_sql, "~> 3.9"},
       {:timex, "~> 3.7"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:ex_machina, "~> 2.7.0", only: :test},
+      {:ex_machina, "~> 2.7", only: :test},
       {:excoveralls, "~> 0.14", only: :test},
-      {:mix_test_watch, "~> 0.8", only: [:test, :dev], runtime: false},
+      {:mix_test_watch, "~> 1.0", only: [:test, :dev], runtime: false},
       {:postgrex, "~> 0.13", only: [:test]},
-      {:rivet_utils, "~> 1.1.5"},
+      {:rivet_utils, "~> 1.2.0"},
       {:transmogrify, "~> 1.1"},
       {:typed_ecto_schema, "~> 0.3.0 or ~> 0.4.1"},
-      {:yaml_elixir, "~> 2.8.0"}
+      {:yaml_elixir, "~> 2.8"}
     ]
   end
 
